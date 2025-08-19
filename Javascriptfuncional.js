5b28f5b807d2040b2b7e1954f3c70caefc4b87fe
Alan Cristian <alan.dias@dcomp.ufs.br>
2025-08-19 08:54:28 -0300

    Update Javascriptfuncional.js
    Consegui resolver os problemas das setas

--- Javascriptfuncional.js
+++ Javascriptfuncional.js
@@ -13,21 +13,21 @@ const dx = [(velocidade[0])]//representa a velocidade no eixo X, ou seja, veloci
 const dy = [-(velocidade[0])]//representa a velocidade no eixo X, ou seja, velocidade na vertical.
 
 //informações que o jogador precisa saber:
-
 const vidas = [3] //quantidade de vidas que o jogador tem.
 const fase = [1] //fase em que o jogador está.
 const pontos = [0] //quantidade de pontos que o jogador possui.
 const recorde = [Number(localStorage.getItem("recorde") ) || 0] //procura se já tinha algo salvo na página com o nome recorde.
 
 //partes que estão relacionadas com a barra controlada palo jogador
-
 const barraAltura = [15]
 const barraLargura = [110]
 const barraX = [(tela.width - barraLargura[0]) / 2]//valor que está relacionado com a posição do jogador
 
-let setaDireita = false;
-let setaEsquerda = false;
+//setas que o jogador usará para controlar a barra
+const setaDireita = [false]
+const setaEsquerda = [false]
 
+//é aqui que o diabo se mostra
 let blocoLinhas = 5;
 let blocoColunas = 10;
 let blocoLargura = 80;
@@ -36,24 +36,26 @@ let blocoPadding = 30;
 let topoMargem = 10;
 let esquerdaMargem = 30;
 
+
+const teclaPressionadaHandler = (e) => {
+    if (e.key === "ArrowRight") {
+        setaDireita[0] = true;
+    } else if (e.key === "ArrowLeft") {
+        setaEsquerda[0] = true;
+    }
+}
+
+const teclaSoltaHandler = (e) => {
+    if (e.key === "ArrowRight") {
+        setaDireita[0] = false;
+    } else if (e.key === "ArrowLeft") {
+        setaEsquerda[0] = false;
+    }
+}
+//tive que mudar a ordem, antes o comando doxument vinha primeiro que as funções de alta ordem que estão ligadas as setas
+//isso causava um problema gigante, pois as constantes ainda não existiam.
 document.addEventListener("keydown", teclaPressionadaHandler);
 document.addEventListener("keyup", teclaSoltaHandler);
-
-function teclaPressionadaHandler(e) {
-    if (e.key === "ArrowRight") {
-        setaDireita = true;
-    } else if (e.key === "ArrowLeft") {
-        setaEsquerda = true;
-    }
-}
-
-function teclaSoltaHandler(e) {
-    if (e.key === "ArrowRight") {
-        setaDireita = false;
-    } else if (e.key === "ArrowLeft") {
-        setaEsquerda = false;
-    }
-}
 
 //inivação que nós fizemos ao código, antes não tinha nenhum som.
 //está funcionando perfeitamente, em time que está ganhando não se mexe.
@@ -71,8 +73,8 @@ const reposicionarBola = () => {
     y[0] = tela.height - 30;
     dx[0] = velocidade[0];
     dy[0] = -(velocidade[0]);
-    setaDireita = false;
-    setaEsquerda = false;
+    setaDireita[0] = false;
+    setaEsquerda[0] = false;
     barraX[0] = (tela.width - barraLargura[0]) / 2;
 }
 //função que gera uma cor aleatória (é usada para criar uma cor para os retângulos que devem ser destruidos).
@@ -82,6 +84,7 @@ const gerarCorAleatoria = () => {
     return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
+//é aqui que a porca torce o rabo
 let blocos = [];
 for (let i = 0; i < blocoColunas; i++) {
     blocos[i] = [];
@@ -97,6 +100,7 @@ for (let i = 0; i < blocoColunas; i++) {
 
 document.getElementById("recordeValor").innerText = recorde[0];
 
+//é aqui que a porca torce o rabo
 function colisaoBlocos() {
     checarProximaFase();
     for (let i = 0; i < blocoColunas; i++) {
@@ -147,6 +151,7 @@ window.onload = function() {
     }
 };
 
+//é aqui que a porca torce o rabo
 function checarProximaFase() {
     let todosBlocosDestruidos = true;
     for (let i = 0; i < blocoColunas; i++) {
@@ -187,6 +192,7 @@ const drawBarra = () => {
     ctx.closePath();
 }
 
+//é aqui que a porca torce o rabo
 function drawBlocos() {
     for (let i = 0; i < blocoColunas; i++) {
         for (let j = 0; j < blocoLinhas; j++) {
@@ -205,6 +211,7 @@ function drawBlocos() {
     }
 }
 
+//é aqui que a porca torce o rabo
 function draw() {
     ctx.clearRect(0, 0, tela.width, tela.height);
     drawBlocos();
@@ -229,10 +236,10 @@ function draw() {
         }
         if (x[0] > barraX[0] + 2 && x[0] < barraX[0] + barraLargura[0] + 2) {
             dy[0] = -dy[0];
-            if(setaDireita === true){
+            if(setaDireita[0] === true){
                 dx[0] = Math.abs(dx[0])+1; 
             }
-            if(setaEsquerda === true){
+            if(setaEsquerda[0] === true){
                 dx[0] = -Math.abs(dx[0])-1;
             }
         } else {//parte modificada e está funcionando.
@@ -244,9 +251,9 @@ function draw() {
             }
         }
     }
-    if (setaDireita && barraX[0] < tela.width - barraLargura[0]) {
+    if (setaDireita[0] && barraX[0] < tela.width - barraLargura[0]) {
         barraX[0] += 10;
-    } else if (setaEsquerda && barraX[0] > 0) {
+    } else if (setaEsquerda[0] && barraX[0] > 0) {
         barraX[0] -= 10;
     }
     requestAnimationFrame(draw);
